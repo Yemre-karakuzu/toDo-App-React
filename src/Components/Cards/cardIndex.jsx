9919df0199b607981896eec81da785ba3554a882
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-09-11 22:24:39 +0300

    delete and checked methode done

--- src/Components/Cards/cardIndex.jsx
+++ src/Components/Cards/cardIndex.jsx
@@ -1,27 +1,37 @@
-import {useState} from 'react'
+// import {useState} from 'react'
 
 function CardIndex({todo,addTodo}) {
-    const [newtodo,setNewTodo]=useState({done:false,text:""});
+    // const [newtodo,setNewTodo]=useState({done:false,text:""});
     const allCheckedBtn =()=>{
         todo.map((item)=>
             item.done=true
         )
     }
+    const deleteTodo=(index)=>{
+        const newTodos = [...todo];
+        newTodos.splice(index, 1);
+        addTodo(newTodos)
+    }
+    const checkedTodo=(index)=>{
+        const newTodos = [...todo];
+        newTodos[index].done = !newTodos[index].done;
+        addTodo(newTodos);
+    }
     return (
         <div className="main">
         <input property="toggleAll" id="toggle-all" className="toggle-all" type="checkbox" checked="[todoLeft = 0]"/>
-		<label htmlFor="toggle-all" onClick={allCheckedBtn}>
+		<label htmlFor="toggle-all" onClick={()=>allCheckedBtn}>
 			Mark all as complete
 		</label>
         <ul className="todo-list">
             {                
                 todo.map((element,i) => {
-                    return <li className={(todo.done ? "completed " : "")} mv-multiple="todo" key={i}>
+                    return <li className={ element.done ? "completed" : "" }  key={i}>
                         {  
                         <div className="view">
-					        <input property="done" className="toggle" type="checkbox"/>
+					        <input property="done" onClick={()=>checkedTodo(i)} className="toggle" type="checkbox"/>
 					        <label property="text">{element.text}</label>
-					        <button className="destroy" mv-action="delete(todo)"></button>
+					        <button className="destroy" onClick={()=>deleteTodo(i)}></button>
 				        </div>                      
                         }
                     </li>
